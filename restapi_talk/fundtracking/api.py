8fe377311ab6d3e89b401ad5293df282b7759e00
Seh Hui, Leong <felixleong@gmail.com>
2013-05-27 19:22:47 +0800

    Updated demo and added filtering.

--- restapi_talk/fundtracking/api.py
+++ restapi_talk/fundtracking/api.py
@@ -1,4 +1,7 @@
+from django.conf.urls.defaults import url
+from tastypie import fields
 from tastypie.resources import ModelResource
+from tastypie.utils import trailing_slash
 from restapi_talk.fundtracking.models import FundHouse, Fund, Price, Annotation
 from restapi_talk.api import v1_api
 
@@ -11,10 +14,25 @@ class FundHouseResource(ModelResource):
 
 
 class FundResource(ModelResource):
+    fund_house = fields.ForeignKey(FundHouseResource, 'fund_house')
+
     class Meta:
         queryset = Fund.objects.all()
         resource_name = 'fund'
         allowed_methods = ['get']
+        detail_uri_name = 'code'
+        filtering = {
+            'fund_house': ['exact', 'in', ],
+        }
+
+    def prepend_urls(self):
+        return [
+            url(
+                r"^(?P<resource_name>%s)/(?P<pk>\d+)%s$" % (
+                    self._meta.resource_name, trailing_slash()),
+                self.wrap_view('dispatch_detail'),
+                name="api_dispatch_detail_pk"),
+        ]
 
 
 class PriceResource(ModelResource):
